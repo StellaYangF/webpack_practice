f96108c90a10f5a48d82a411eab432c4c9f1f7b5
StellaYangF <1062878468@qq.com>
2020-03-11 23:15:25 +0800

    friendly-errors and bundle-analyzer config added

--- advanced_config/src/index.js
+++ advanced_config/src/index.js
@@ -4,9 +4,15 @@
 // 全局变量
 // import 方法无效
 // require("expose-loader?_!lodash");
-
+import "@/index.css";
+// import "bootstrap";
 import _ from 'lodash';
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 
 
 console.log(_.join(['a', 'b', 'c'], '@'));
+
+console.log(PRODUCTION);
+console.log(VERSION);
+console.log(EXPRESSION);
+console.log(COPYRIGHT);
--- advanced_config/webpack.config.js
+++ advanced_config/webpack.config.js
@@ -3,17 +3,21 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const SpeedMeasureWebpackPlugin = require('speed-measure-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const smw = new SpeedMeasureWebpackPlugin();
 const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const resolvePath = filePath => path.resolve(__dirname, filePath);
 
 module.exports = env => {
-    return {
+    return smw.wrap({
         mode: env,
         entry: resolvePath('./src'),
 
         // watch
-        watch: true,
+        // watch: true,
         // watchOptions: {
         //     ignored: /node_modules/,
         //     aggregateTimeout: 300,
@@ -40,6 +44,7 @@ module.exports = env => {
             path: resolvePath('./dist'),
         },
         module: {
+            noParse: /lodash/,
             rules: [
                 {
                     test: /\.css$/,
@@ -54,12 +59,14 @@ module.exports = env => {
                     test: /\.js$/,
                     enforce: 'pre',
                     include: [resolvePath('src')],
-                    use: [{
-                        loader: 'eslint-loader',
-                        options: {
-                            fix: true
-                        }
-                    }, 'babel-loader'],
+                    use: [
+                    //     {
+                    //     loader: 'eslint-loader',
+                    //     options: {
+                    //         fix: true
+                    //     }
+                    // }, 
+                    'babel-loader'],
                 },
                 {
                     test: /\.(woff|ttf|eot|svg|otf)$/,
@@ -104,10 +111,36 @@ module.exports = env => {
             // copyright
             new webpack.BannerPlugin('乡聚旅游'),
 
-          
+            // DefinePlugin
+            new webpack.DefinePlugin({
+                PRODUCTION: JSON.stringify(true),
+                VERSION: '1',
+                EXPRESSION: "1+2",
+                COPYRIGHT: {
+                    AUTHOR: JSON.stringify("乡聚旅游"),
+                }
+            }),
+
+            new FriendlyErrorsWebpackPlugin(),
+
+            new BundleAnalyzerPlugin(),
         ],
+        // externals
         externals: {
             lodash: '_',
-        }
-    }
+        },
+
+        // resolve
+        resolve: {
+            extensions: ['.js', '.jsx', '.json', '.css'],
+            alias: {
+                "@": resolvePath('./src'),
+                bootstrap$: resolvePath("./node_modules/_bootstrap@3.3.7@bootstrap/dist/css/bootstrap.css"),
+            },
+            mainFields: ['browser', 'module', 'main'],
+        },
+
+        // friendly-errors
+        stats: 'minimal',
+    })
 };
